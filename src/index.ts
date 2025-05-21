79600384a0247f6bafefd06af9e36d0cb3aec3a7
Alexandre Neuwald <alexandre.neuwald@criticaltechworks.com>
2025-05-21 06:24:40 +0100

    move types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
-import { ExtensionContext } from "@lichtblick/suite";
 import { CylinderCameraModel } from "./CylinderCameraModel";
-import { CameraInfo, ICameraModel } from "./lichtblick-suite.types";
-
-type CameraModelBuilder = (CameraInfo: CameraInfo) => ICameraModel;
-
-type ExtendedExtensionContext = ExtensionContext & {
-  registerCameraModel(name: string, builder: CameraModelBuilder): void;
-};
+import { CameraInfo, ExtendedExtensionContext } from "./lichtblick-suite.types";
 
 export function activate(extensionContext: ExtendedExtensionContext): void {
   extensionContext.registerCameraModel(
